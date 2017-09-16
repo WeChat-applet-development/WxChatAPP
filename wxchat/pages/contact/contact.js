d17aad1acb0cd209a4cc8cd2a2542fd69799917a
李辉道 <1069945237@qq.com>
2017-09-16 08:22:51 +0800

    关于我们中增加地图导航功能
    关于我们中增加地图导航功能

--- wxchat/pages/contact/contact.js
+++ wxchat/pages/contact/contact.js
@@ -3,6 +3,23 @@ var WxParse = require('../../utils/wxParse/wxParse.js');
 
 Page({
   data: {
+    latitude: 23.099994,
+    longitude: 113.324520,
+    markers: [{
+      latitude: 23.099994,
+      longitude: 113.324520,
+      name: '商家产品发布平台'
+    }],
+    covers: [{
+      latitude: 23.099994,
+      longitude: 113.344520,
+      iconPath: '/images/green_tri.png', // 目前有 bug，正确的写法应该是 /image/green_tri.png ，等我们下个版本修复吧T_T 
+    }, {
+      latitude: 23.099994,
+      longitude: 113.304520,
+      iconPath: '/images/green_tri.png',
+      rotate: 180
+    }]
   },
   onLoad: function () {
     var that = this;
