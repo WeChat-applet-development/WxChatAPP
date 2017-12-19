366713e94bcc753a62d9f5efb662c7ee39609ae3
李辉道 <1069945237@qq.com>
2017-12-20 01:56:01 +0800

    增加师资列表API
    增加师资列表API

--- wxchatapp/src/Topxia/MobileBundleV2/Controller/TeacherAppController.php
+++ wxchatapp/src/Topxia/MobileBundleV2/Controller/TeacherAppController.php
@@ -6,10 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Topxia\WebBundle\Controller\BaseController;
 use Symfony\Component\HttpFoundation\Request;
 use Topxia\Common\ArrayToolkit;
+use Topxia\Service\Util\CloudClientFactory;
 
 class TeacherAppController extends MobileBaseController{
 
-	public function indexAction(){
+	/*public function indexAction(){
         $conditions = array(
             'roles'=>'ROLE_TEACHER',
             'locked'=>0
@@ -36,5 +37,34 @@ class TeacherAppController extends MobileBaseController{
             'teachers' => $teachers ,
             'profiles' => $profiles
 		));
+	}*/
+	
+	public function indexAction(Request $request){
+        $start = (int) $request->get("start", 0);
+        $limit = (int) $request->get("limit", 10);
+		
+		$conditions = array(
+            'roles'=>'ROLE_TEACHER',
+            'locked'=>0
+        );
+
+		$result = array();
+		$result['status'] = $request->query->get('status', "ok");
+		$result['count'] = $limit;
+		$result['total'] = $this->getUserService()->searchUserCount($conditions);
+        $result['start'] = $start;
+        $result['limit'] = $limit;
+				
+		$teachers = $this->getUserService()->searchUsers(
+            $conditions,
+            array('promotedTime', 'DESC'),
+            $start,
+            $limit
+        );
+		
+		$result['profiles'] = $profiles = $this->getUserService()->findUserProfilesByIds(ArrayToolkit::column($teachers, 'id'));
+        $result['teachers'] = $teachers = $this->filterUsers($teachers);
+
+        return $this->createJson($request, $result);
 	}
 }
